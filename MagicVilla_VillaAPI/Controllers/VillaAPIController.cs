2050e0f9e507b4770df32326b5b3c8b2a67adbdb
VijayHYadav <vizzyadav1997@gmail.com>
2023-05-04 07:08:55 +0530

    003 Create VillaAPIController Class

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+    }
+}
+
+
+/*
+ * In order to make that a controller,it must end with the word controller. 
+ * In order to make this a controller class, it has to derive from the class controller base.
+ * 
+ * If you are creating a REST API, make your Controller class derive from ControllerBase and not Controller class. Controller derives from ControllerBase and adds support for MVC views. 
+ */
