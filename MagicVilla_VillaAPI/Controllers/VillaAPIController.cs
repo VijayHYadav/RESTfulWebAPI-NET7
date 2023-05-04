54bfea7a2973e49031638ce0d61feb7af084c40d
VijayHYadav <vizzyadav1997@gmail.com>
2023-05-04 07:35:31 +0530

    008 Get individual Villa

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -15,6 +15,12 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("id:int")]
+        public VillaDTO? GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
     }
 }
 
@@ -23,5 +29,7 @@ namespace MagicVilla_VillaAPI.Controllers
  * In order to make that a controller,it must end with the word controller. 
  * In order to make this a controller class, it has to derive from the class controller base.
  * 
- * If you are creating a REST API, make your Controller class derive from ControllerBase and not Controller class. Controller derives from ControllerBase and adds support for MVC views. 
+ * If you are creating a REST API, make your Controller class derive from ControllerBase and not Controller class. Controller derives from ControllerBase and adds support for MVC views. .
+ * 
+ * If you do not define HTTP verb, it defaults to "HTTPGET"
  */
