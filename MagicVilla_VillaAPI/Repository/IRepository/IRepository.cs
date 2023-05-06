56e3fa84b0088de6d9e612676a33356f5473ca38
VijayHYadav <vizzyadav1997@gmail.com>
2023-05-06 07:37:45 +0530

    004 Repository CleanUp

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class // where T : class says that T will be of type class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task SaveAsync();
+    }
+}
