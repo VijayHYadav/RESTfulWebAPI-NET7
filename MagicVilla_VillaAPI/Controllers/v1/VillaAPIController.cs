19a244f401d970683bd3bad1d6efcf9698f7922d
VijayHYadav <vizzyadav1997@gmail.com>
2023-05-07 08:18:03 +0530

    004 Seach Villa Name

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -41,7 +41,7 @@ namespace MagicVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")]int? occupancy)
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")]int? occupancy, [FromQuery] string? search)
         {
             try
             {
@@ -54,6 +54,11 @@ namespace MagicVilla_VillaAPI.Controllers.v1
                 } else
                 {
                     villaList = await _dbVilla.GetAllAsync();
+                }
+
+                if (!string.IsNullOrEmpty(search))
+                {
+                    villaList = villaList.Where(u.Name.ToLower().Contains(search));
                 }
 
                 _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
